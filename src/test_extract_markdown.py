9995ea1cb09e4ca6a8a3135e632ffe903d931b50
Karl Hans Laurits <karl@laurits.dev>
2025-02-05 16:32:02 +0200

    Add markdown image and link extractor functions

---
+++ src/test_extract_markdown.py
@@ -0,0 +1,27 @@
+import unittest
+
+from extract_markdown import extract_markdown_images, extract_markdown_links
+
+
+class TestExtractMarkdownImages(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        expected_output = [
+            ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+            ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+        ]
+        self.assertEqual(extract_markdown_images(text), expected_output)
+
+
+class TestExtractMarkdownLinks(unittest.TestCase):
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        expected_output = [
+            ("to boot dev", "https://www.boot.dev"),
+            ("to youtube", "https://www.youtube.com/@bootdotdev"),
+        ]
+        self.assertEqual(extract_markdown_links(text), expected_output)
+
+
+if __name__ == "__main__":
+    unittest.main()
