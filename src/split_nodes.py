f58807070ed5c3c5daf347809b823c61332707a3
Karl Hans Laurits <karl@laurits.dev>
2025-02-08 16:54:15 +0200

    Implement split_nodes_image and split_nodes_link functions and tests

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -21,5 +21,68 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 old_node_parts.append(TextNode(old_node_text_parts[1], TextType.CODE))
             old_node_parts.append(TextNode(old_node_text_parts[2], TextType.TEXT))
             new_nodes.extend(old_node_parts)
+    return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        current_text = old_node.text
+        matches = extract_markdown_images(current_text)
+
+        if not current_text:
+            continue
+        if not matches:
+            new_nodes.append(old_node)
+            continue
+        for match in matches:
+            image_alt, image_link = match
+            image_markdown = f"![{image_alt}]({image_link})"
+            parts = current_text.split(image_markdown, 1)
+
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+
+            if len(parts) > 1:
+                current_text = parts[1]
+            else:
+                current_text = ""
+
+        if current_text:
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        current_text = old_node.text
+        matches = extract_markdown_links(current_text)
+
+        if not current_text:
+            continue
+        if not matches:
+            new_nodes.append(old_node)
+            continue
+        for match in matches:
+            link_text, link = match
+            link_markdown = f"[{link_text}]({link})"
+            parts = current_text.split(link_markdown, 1)
+
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(link_text, TextType.LINK, link))
+
+            if len(parts) > 1:
+                current_text = parts[1]
+            else:
+                current_text = ""
+
+        if current_text:
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
 
     return new_nodes
--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -16,5 +16,65 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         self.assertEqual(new_nodes, expected_output)
 
 
+class TestSplitNodesImage(unittest.TestCase):
+    def test_split_nodes_image(self):
+        node = TextNode("This is text with an image ![to boot dev](https://www.boot.dev)", TextType.TEXT)
+        nodes = split_nodes_image([node])
+        self.assertEqual(len(nodes), 2)
+        self.assertEqual(nodes[0].text, "This is text with an image ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "to boot dev")
+        self.assertEqual(nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(nodes[1].url, "https://www.boot.dev")
+
+    def test_split_nodes_image_multiple_images(self):
+        node = TextNode(
+            "This is text with an image ![to boot dev](https://www.boot.dev) and ![to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        nodes = split_nodes_image([node])
+        self.assertEqual(len(nodes), 4)
+        self.assertEqual(nodes[0].text, "This is text with an image ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "to boot dev")
+        self.assertEqual(nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(nodes[1].url, "https://www.boot.dev")
+        self.assertEqual(nodes[2].text, " and ")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(nodes[3].text, "to youtube")
+        self.assertEqual(nodes[3].text_type, TextType.IMAGE)
+        self.assertEqual(nodes[3].url, "https://www.youtube.com/@bootdotdev")
+
+
+class TestSplitNodesLink(unittest.TestCase):
+    def test_split_nodes_link(self):
+        node = TextNode("This is text with a link [to boot dev](https://www.boot.dev)", TextType.TEXT)
+        nodes = split_nodes_link([node])
+        self.assertEqual(len(nodes), 2)
+        self.assertEqual(nodes[0].text, "This is text with a link ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "to boot dev")
+        self.assertEqual(nodes[1].text_type, TextType.LINK)
+        self.assertEqual(nodes[1].url, "https://www.boot.dev")
+
+    def test_split_nodes_link_multiple_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        nodes = split_nodes_link([node])
+        self.assertEqual(len(nodes), 4)
+        self.assertEqual(nodes[0].text, "This is text with a link ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "to boot dev")
+        self.assertEqual(nodes[1].text_type, TextType.LINK)
+        self.assertEqual(nodes[1].url, "https://www.boot.dev")
+        self.assertEqual(nodes[2].text, " and ")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(nodes[3].text, "to youtube")
+        self.assertEqual(nodes[3].text_type, TextType.LINK)
+        self.assertEqual(nodes[3].url, "https://www.youtube.com/@bootdotdev")
+
+
 if __name__ == "__main__":
     unittest.main()
