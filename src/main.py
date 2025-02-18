236ff7cdfc1c731ba20d7761c6cd21ec546de9fb
Karl Hans Laurits <karl@laurits.dev>
2025-02-18 21:09:52 +0200

    Implement auto-creation of public from static

--- src/main.py
+++ src/main.py
@@ -1,9 +1,32 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    public = os.path.join(project_root, 'public')
+    static = os.path.join(project_root, 'static')
+    if os.path.exists(public):
+        shutil.rmtree(public)
+    copy_static_to_public(public, static)
+    print(os.listdir(public))
 
 
-main()
+def copy_static_to_public(public_path, static_path):
+    if not os.path.exists(public_path):
+        os.mkdir(public_path)
+    for item in os.listdir(static_path):
+        dst_item = os.path.join(public_path, item)
+        src_item = os.path.join(static_path, item)
+        if os.path.isfile(src_item):
+            print(f"Copying {src_item} to {dst_item}")
+            shutil.copy2(src_item, dst_item)
+        elif os.path.isdir(src_item):
+            print(f"Copying folder {src_item} to {dst_item}")
+            os.mkdir(dst_item)
+            copy_static_to_public(dst_item, src_item)
+            # shutil.copytree(src_item, dst_item)  # This replaces the recursive call
+
+
+if __name__ == '__main__':
+    main()
