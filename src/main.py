32aca533948929f987424ddb85a1aea716219db5
Karl Hans Laurits <karl@laurits.dev>
2025-02-28 02:37:29 +0200

    Implement recursive page generation and more pages

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 
+base_path = sys.argv[1] if len(sys.argv) > 1 else "/"
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
@@ -20,7 +22,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, base_path)
 
 
 if __name__ == "__main__":
